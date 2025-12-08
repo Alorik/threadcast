b318c9058f6ee92754021dc742e366b37c878e9d
NITIN KIROLA <nitinkirola1701@gmail.com>
2025-12-08 10:59:52 +0530

    feat: zod validation for posts added

--- schema/auth.ts
+++ schema/auth.ts
@@ -3,10 +3,17 @@ import { z } from "zod";
 export const RegisterSchema = z.object({
   email: z.string().email(),
   username: z.string().min(5).max(30),
-  password: z.string().min(7).max(20)
+  password: z.string().min(7).max(20),
 });
 
 export const LoginSchema = z.object({
   email: z.string().min(5).max(30),
-  password: z.string().min(7).max(20)
+  password: z.string().min(7).max(20),
+});
+
+export const PostSchema = z.object({
+  content: z
+    .string()
+    .min(1, "Post cannot be empty")
+    .max(280, "cannot be longer than 280 words"),
 });
