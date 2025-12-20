c438c6e1f499b6be381c526806ca05c16804f4b9
NITIN KIROLA <nitinkirola1701@gmail.com>
2025-12-20 19:37:12 +0530

    feat: presence offline api added

---
+++ app/api/presence/offline/route.ts
@@ -0,0 +1,18 @@
+import { authOptions } from "@/auth/config";
+import { pusherServer } from "@/lib/pusher-server";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const userId = session.user.id;
+
+  await pusherServer.trigger("presence", "user-offline", {
+    userId,
+  });
+  return NextResponse.json({ success: true });
+}
