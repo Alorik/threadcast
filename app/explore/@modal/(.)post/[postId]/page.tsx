4a196f3a99f1e7f9ce11462a8e7b816ac8bb5a85
NITIN KIROLA <nitinkirola1701@gmail.com>
2025-12-25 23:45:36 +0530

    @modal postId added

---
+++ app/explore/@modal/(.)post/[postId]/page.tsx
@@ -0,0 +1,18 @@
+import ExplorePostModal from "@/components/explore/page";
+import { prisma } from "@/lib/prisma";
+
+export default async function Page({ params }: { params: { postId: string } }) {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: params.postId,
+    },
+    include: {
+      user: true,
+      media: true,
+      _count: { select: { likes: true } },
+    },
+  });
+
+  if (!post) return null;
+  return <ExplorePostModal post={post} />;
+}
